b9c0e312f9eecd91e2999ddefc335e206f8995b0
padi-dev-quanph <phanhongquan07122003@gmail.com>
2025-05-25 14:59:09 +0700

    loading circle

--- lib/ui/common/widgets/loading_circle.dart
+++ lib/ui/common/widgets/loading_circle.dart
@@ -1,16 +1,28 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:toeic_desktop/ui/common/app_colors.dart';
 
 class LoadingCircle extends StatelessWidget {
-  const LoadingCircle({super.key});
+  const LoadingCircle({
+    super.key,
+    this.size = 48,
+    this.color = AppColors.primary,
+    this.strokeWidth = 2,
+  });
+
+  final double size;
+  final Color color;
+  final double strokeWidth;
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: CircularProgressIndicator(
-        color: AppColors.primary,
+    return SizedBox(
+      width: size,
+      height: size,
+      child: Center(
+        child: CircularProgressIndicator(
+          color: color,
+          strokeWidth: strokeWidth,
+        ),
       ),
     );
   }
