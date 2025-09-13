a33428d59d0050965e1f6e54c331842ccd565961
Winter <phanhongquan07122003@gmail.com>
2025-09-14 00:54:26 +0700

    Enhance PrepareLiveHeader with user information and view count display
    Updated the PrepareLiveHeader widget to include user avatar and name using Bloc for state management. Added a view count parameter to display the number of viewers, improving the user interface and providing real-time feedback during live sessions. Refactored the layout for better organization and maintainability, ensuring a cleaner code structure.

--- lib/ui/page/prepare_live/widgets/prepare_live_header.dart
+++ lib/ui/page/prepare_live/widgets/prepare_live_header.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:toeic_desktop/common/configs/app_configs.dart';
+import 'package:toeic_desktop/common/global_blocs/user/user_cubit.dart';
+import 'package:toeic_desktop/data/models/entities/profile/user_entity.dart';
 import 'package:toeic_desktop/language/generated/l10n.dart';
 import 'package:toeic_desktop/ui/common/app_context.dart';
 import 'package:toeic_desktop/ui/common/widgets/confirm_dia_log.dart';
 
 class PrepareLiveHeader extends StatelessWidget {
   final Function()? onClose;
-  const PrepareLiveHeader({super.key, this.onClose});
+  final int viewCount;
+  const PrepareLiveHeader({super.key, this.onClose, this.viewCount = 0});
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = context.colorScheme;
     final textTheme = context.textTheme;
     return Container(
       height: 50,
@@ -20,21 +24,48 @@ class PrepareLiveHeader extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
             decoration: BoxDecoration(
-                color: colorScheme.primary.withValues(alpha: 0.2),
+                // color: colorScheme.primary.withValues(alpha: 0.2),
                 borderRadius: BorderRadius.circular(50)),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 10,
-                  backgroundImage:
-                      Image.network('https://picsum.photos/200/300').image,
-                ),
-                const SizedBox(width: 4),
-                Text('Live Stream',
-                    style: textTheme.bodySmall?.copyWith(color: Colors.white)),
-              ],
+            child: BlocSelector<UserCubit, UserState, UserEntity?>(
+              selector: (state) {
+                return state.user;
+              },
+              builder: (context, user) {
+                return Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 18,
+                      backgroundImage: Image.network(
+                              '${AppConfigs.baseUrl}${user?.avatar ?? ''}')
+                          .image,
+                    ),
+                    const SizedBox(width: 4),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(user?.name ?? 'No Name',
+                            style: textTheme.titleSmall
+                                ?.copyWith(color: Colors.white, fontSize: 12)),
+                        // number of view
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Icon(Icons.remove_red_eye, size: 12),
+                            const SizedBox(width: 4),
+                            Text(viewCount.toString(),
+                                style: textTheme.bodySmall?.copyWith(
+                                    color: Colors.white, fontSize: 12)),
+                          ],
+                        )
+                      ],
+                    ),
+                    const SizedBox(width: 16),
+                  ],
+                );
+              },
             ),
           ),
           IconButton(
